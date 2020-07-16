bbcb02d4d56b4619a910ac62b763d7ebb35c671f
nikunj-26 <nikunj.purohit@sakec.ac.in>
2020-07-16 18:28:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNum}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNum);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNum: 1),
+              buildKey(color: Colors.orange, soundNum: 2),
+              buildKey(color: Colors.yellow, soundNum: 3),
+              buildKey(color: Colors.green, soundNum: 4),
+              buildKey(color: Colors.teal, soundNum: 5),
+              buildKey(color: Colors.blue, soundNum: 6),
+              buildKey(color: Colors.deepPurple, soundNum: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
